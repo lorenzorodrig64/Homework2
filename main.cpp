83620c4d521ac148be0d03b8c3e20d7461e22bf5
lorenzorodrig64 <lbr5353@psu.edu>
2024-04-06 10:29:45 -0400

    Question 4 outline

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+// #include <GL/glew.h>
+// #include <GLFW/glfw3.h>
+#include <cmath>
 #include "Sensor.h"
 #include "SensorFactory.h"
 #include "AerospaceControlSystem.h"
@@ -37,4 +40,103 @@ int main()
 
 	// end of question 2
 
+	// start of question 3
+
+	std::cout << "########## START OF QUESTION 3 ##########" << std::endl;
+	std::cout << std::endl;
+
+
+
+	std::cout << std::endl;
+	std::cout << "########## END OF QUESTION 3 ##########" << std::endl;
+	std::cout << std::endl;
+
+	// end of question 3
+
+	// start of question 4
+
+	std::cout << "########## START OF QUESTION 4 ##########" << std::endl;
+	std::cout << std::endl;
+
+	//if (!glfwInit()) {
+	//	std::cerr << "Failed to initialize GLFW" << std::endl;
+	//	return -1;
+	//}
+
+	//GLFWwindow* window = glfwCreateWindow(500, 500, "Sine Curve using OpenGL with GLEW and GLFW", NULL, NULL);
+	//if (!window) {
+	//	std::cerr << "Failed to create GLFW window" << std::endl;
+	//	glfwTerminate();
+	//	return -1;
+	//}
+
+	//glfwMakeContextCurrent(window);
+
+	//if (glewInit() != GLEW_OK) {
+	//	std::cerr << "Failed to initialize GLEW" << std::endl;
+	//	return -1;
+	//}
+
+	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	//while (!glfwWindowShouldClose(window)) {
+	//	glClear(GL_COLOR_BUFFER_BIT);
+
+	//	glBegin(GL_LINES);
+	//	// Draw X and Y axis
+	//	glColor3f(1.0, 1.0, 1.0);
+	//	glVertex2f(-1.0, 0.0);
+	//	glVertex2f(1.0, 0.0);
+	//	glVertex2f(0.0, 1.0);
+	//	glVertex2f(0.0, -1.0);
+
+	//	// Plot sine curve
+	//	glColor3f(0.0, 1.0, 0.0);
+	//	for (float i = 0; i < 2 * 3.14159; i += 0.001) {
+	//		float x = (2 * i - 3.14159) / 3.14159;
+	//		float y = sin(i);
+	//		glVertex2f(x, y - 0.001);
+	//		glVertex2f(x, y);
+	//	}
+	//	glEnd();
+
+	//	glfwSwapBuffers(window);
+	//	glfwPollEvents();
+	//}
+
+	//glfwTerminate();
+
+	std::cout << std::endl;
+	std::cout << "########## END OF QUESTION 4 ##########" << std::endl;
+	std::cout << std::endl;
+
+	// end of question 4
+
+	// start of question 5
+
+	std::cout << "########## START OF QUESTION 5 ##########" << std::endl;
+	std::cout << std::endl;
+
+
+
+	std::cout << std::endl;
+	std::cout << "########## END OF QUESTION 5 ##########" << std::endl;
+	std::cout << std::endl;
+
+	// end of question 5
+
+	// start of question 6
+
+	std::cout << "########## START OF QUESTION 6 ##########" << std::endl;
+	std::cout << std::endl;
+
+
+
+	std::cout << std::endl;
+	std::cout << "########## END OF QUESTION 6 ##########" << std::endl;
+	std::cout << std::endl;
+
+	// end of question 6
+
+	return 0;
 }
