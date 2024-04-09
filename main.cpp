7c0a26044abdbccf45b263ebb8976b0dcb4e0bc3
lorenzorodrig64 <lbr5353@psu.edu>
2024-04-08 22:18:10 -0400

    Questions 2 through 4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-// #include <GL/glew.h>
-// #include <GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <cmath>
 #include "Sensor.h"
 #include "SensorFactory.h"
@@ -9,22 +9,91 @@
 #include <chrono>
 #include <mutex>
 
-std::mutex robotOrder; 
-// this mutex variable makes sure that the output for simultaneous tasks in question 2 do not get mixed
+std::mutex output; 
+// this mutex variable is used throughtout questions 2 and 3 in order to prevent simultaneous outputs to be printed at the same time.
+std::mutex tools[5];
+// this mutex variable represents the 5 tools in between the 5 robots (question 2)
 
-void robot_task(int robot_number) // this function takes in the number of the robot and performs its task
+void robot_task(int robot_number) // this function takes in the number of the robot and performs its task (question 2)
 {
-	robotOrder.lock(); // locking to make sure outputs do not get mixed for simulataneous tasks
+	int leftTool = robot_number;
+	int rightTool = (robot_number + 1) % 5;
+	// the calculation for the number of the right tool makes sure it is never adjacent to the left tool
+
+	if (leftTool > rightTool) std::swap(leftTool, rightTool);
+	// this will prevent deadlocking by making sure the lower-index tool gets locked first
+
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	// time each robot takes to reach for tools
+
+	std::lock(tools[leftTool], tools[rightTool]); 
+	// robot acquires adjacent tools. Locking mutexes of adjacent tools to robot prevents adjacent robots to work at the same time
+
+	std::lock_guard<std::mutex> lockLeft(tools[leftTool], std::adopt_lock);
+	std::lock_guard<std::mutex> lockRight(tools[rightTool], std::adopt_lock);
+	// take possession of locks of corresponding tools to make sure that the tools are unlocked after end of thread execution
+
+	output.lock(); // locking to make sure outputs do not get mixed for simulataneous tasks
 	std::cout << "Robot " << robot_number << " is collection data." << std::endl;
-	robotOrder.unlock(); // unlocking so following task can display output
-	std::this_thread::sleep_for(std::chrono::seconds(1)); // takes robot 1 second to grab tools.
-	robotOrder.lock();
+	output.unlock(); // unlocking so following task can display output
+	output.lock();
 	std::cout << "Robot " << robot_number << " acquired tools and starts performing a task." << std::endl;
-	robotOrder.unlock();
+	output.unlock();
 	std::this_thread::sleep_for(std::chrono::seconds(5)); // takes robot 5 seconds to perform task and return tools.
-	robotOrder.lock();
+	output.lock();
 	std::cout << "Robot " << robot_number << " finished the task and has returned tools." << std::endl;
-	robotOrder.unlock();
+	output.unlock();
+}
+
+std::mutex atcCommLock; // mutex for locking ATC communications
+std::condition_variable wakeUp; // condition variable used to wake up ATC
+std::atomic<int> aircraftInPattern(0); // counts aircrafts currently inside traffic pattern
+
+void attemptToLand(int aircraftNum) 
+{
+	output.lock(); // output lock to make sure that only one airplane can request to land at the time
+	std::cout << "Aircraft #" << aircraftNum << " requesting to land." << std::endl; // initial request to land
+	output.unlock();
+
+	std::unique_lock<std::mutex> lock(atcCommLock); // lock for ATC communication
+
+	if (aircraftInPattern >= 3) // if statement which redirects aircraft to another airport if traffic pattern full
+	{
+		output.lock();
+		std::cout << "Traffic pattern full. Aircraft #" << aircraftNum << " redirected to another airport." << std::endl;
+		output.unlock();
+		return; // thread exits function since it is redirected to another airport
+	}
+
+	if (aircraftInPattern == 0)	// if statement which initially wakes up ATC
+	{
+		wakeUp.notify_one(); // wake up ATC
+		output.lock();
+		std::cout << "Aircraft " << aircraftNum << " is waking up the ATC." << std::endl;
+		output.unlock();
+		output.lock();
+		std::cout << "Aircraft #" << aircraftNum << " in traffic pattern." << std::endl;
+		output.unlock();
+	}
+	else 
+	{
+		output.lock();
+		std::cout << "Aircraft #" << aircraftNum << " in traffic pattern." << std::endl;
+		output.unlock();
+	}
+
+	aircraftInPattern++; // increments planes in traffic pattern
+	lock.unlock(); // unlock ATC lock to allow other pilots to communicate
+
+	std::this_thread::sleep_for(std::chrono::seconds(1)); // time for planes to land
+
+	lock.lock(); // re-use ATC lock to ensure proper landing by planes
+	aircraftInPattern--;
+	std::cout << "Aircraft #" << aircraftNum << " has landed." << std::endl;
+	if (aircraftInPattern == 0) 
+	{
+		std::cout << "ATC goes back to sleep." << std::endl; // last aircraft has landed so ATC can go back to sleep
+	}
 }
 
 int main()
@@ -54,38 +123,26 @@ int main()
 	std::cout << "########## START OF QUESTION 2 ##########" << std::endl;
 	std::cout << std::endl;
 
-	/*
-	This problem asks to create a code which recreates the fastest time for the robots to finish their tasks given
-	the conditions stated in the problem. The shortest time possible for the robots to finish the tasks is 18
-	seconds. This is because given that there are 5 robots in a circle and they each have a tool in between them
-	and they need two tools to perform ther task, this means that only two robots who are not adjacent to each 
-	other can be working on the task simultaneously. If we label the robots 1 to 5 around the circle, this would
-	mean that for the most optimal time to finish all the tasks, robots 1 and 3 would work first simultaneously,
-	then 2 and 4, and finally robot 5. Since each task takes 6 seconds total (including getting and returning 
-	tools). This would mean that the total time would be 6 seconds + 6 seconds + 6 seconds = 18 seconds.
-	*/
+	auto startTimeQ2 = std::chrono::high_resolution_clock::now(); // starting time of question
 
-	auto startTime = std::chrono::high_resolution_clock::now(); // starting time of question
+	std::thread robot1(robot_task, 0);
+	std::thread robot3(robot_task, 1);
+	std::thread robot2(robot_task, 2);
+	std::thread robot4(robot_task, 3);
+	std::thread robot5(robot_task, 4);
+	// declaration of threads representing each robot with their task and number
+	// numbering starts at zero in order for tool locking to work
 
-	std::thread robot1(robot_task, 1); // creation of task for robot 1
-	std::thread robot3(robot_task, 3); // creation of task for robot 3
+	robot1.join();
+	robot2.join();
+	robot3.join();
+	robot4.join(); 
+	robot5.join();
+	// robot threads join main thread
 
-	robot1.join(); // end of task for robot 1
-	robot3.join(); // end of task for robot 3
-
-	std::thread robot2(robot_task, 2); // creation of task for robot 2
-	std::thread robot4(robot_task, 4); // creation of task for robot 4
-
-	robot2.join(); // end of task for robot 2
-	robot4.join(); // end of task for robot 4
-
-	std::thread robot5(robot_task, 5); // creation of task for robot 5
-
-	robot5.join(); // end of task for robot 5
-
-	auto stopTime = std::chrono::high_resolution_clock::now(); // stopping time of querstion
-	auto duration = std::chrono::duration_cast<std::chrono::seconds>(stopTime - startTime); // calculation of duration
-	std::cout << "duration : " << duration.count() << " seconds" << std::endl; // displaying duration
+	auto stopTimeQ2 = std::chrono::high_resolution_clock::now(); // stopping time of querstion
+	auto durationQ2 = std::chrono::duration_cast<std::chrono::seconds>(stopTimeQ2 - startTimeQ2); // calculation of duration
+	std::cout << "duration : " << durationQ2.count() << " seconds" << std::endl; // displaying duration
 
 	std::cout << std::endl;
 	std::cout << "########## END OF QUESTION 2 ##########" << std::endl;
@@ -98,7 +155,21 @@ int main()
 	std::cout << "########## START OF QUESTION 3 ##########" << std::endl;
 	std::cout << std::endl;
 
+	auto startTimeQ3 = std::chrono::high_resolution_clock::now(); // starting time of question
+	
+	std::vector<std::thread> airplanes; // vector to contain airplane threads
 
+	for (int i = 1; i <= 10; ++i) {
+		airplanes.emplace_back(attemptToLand, i); // adds 10 different threads to vector
+	}
+
+	for (auto& t : airplanes) {
+		t.join(); // executes all the threads inside the vector
+	}
+
+	auto stopTimeQ3 = std::chrono::high_resolution_clock::now(); // stopping time of querstion
+	auto durationQ3 = std::chrono::duration_cast<std::chrono::seconds>(stopTimeQ3 - startTimeQ3); // calculation of duration
+	std::cout << "duration : " << durationQ3.count() << " seconds" << std::endl; // displaying duration
 
 	std::cout << std::endl;
 	std::cout << "########## END OF QUESTION 3 ##########" << std::endl;
@@ -111,57 +182,58 @@ int main()
 	std::cout << "########## START OF QUESTION 4 ##########" << std::endl;
 	std::cout << std::endl;
 
-	//if (!glfwInit()) {
-	//	std::cerr << "Failed to initialize GLFW" << std::endl;
-	//	return -1;
-	//}
+	std::cout << "A section of the simple harmonic motion equation will be displayed in a new window. " << std::endl;
 
-	//GLFWwindow* window = glfwCreateWindow(500, 500, "Sine Curve using OpenGL with GLEW and GLFW", NULL, NULL);
-	//if (!window) {
-	//	std::cerr << "Failed to create GLFW window" << std::endl;
-	//	glfwTerminate();
-	//	return -1;
-	//}
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
 
-	//glfwMakeContextCurrent(window);
+	GLFWwindow* window = glfwCreateWindow(500, 500, "Sine Curve using OpenGL with GLEW and GLFW", NULL, NULL);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-	//if (glewInit() != GLEW_OK) {
-	//	std::cerr << "Failed to initialize GLEW" << std::endl;
-	//	return -1;
-	//}
+	glfwMakeContextCurrent(window);
 
-	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
 
-	//while (!glfwWindowShouldClose(window)) {
-	//	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-	//	glBegin(GL_LINES);
-	//	// Draw X and Y axis
-	//	glColor3f(1.0, 1.0, 1.0);
-	//	glVertex2f(-1.0, 0.0);
-	//	glVertex2f(1.0, 0.0);
-	//	glVertex2f(0.0, 1.0);
-	//	glVertex2f(0.0, -1.0);
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
 
-	//	// Plot sine curve
-	//	glColor3f(0.0, 1.0, 0.0);
-	//	for (float i = 0; i < 2 * 3.14159; i += 0.001) {
-	//		float x = (2 * i - 3.14159) / 3.14159;
-	//		float y = sin(i);
-	//		glVertex2f(x, y - 0.001);
-	//		glVertex2f(x, y);
-	//	}
-	//	glEnd();
+		glBegin(GL_LINES);
+		// Draw X and Y axis
+		glColor3f(1.0, 1.0, 1.0);
+		glVertex2f(-1.0, 0.0);
+		glVertex2f(1.0, 0.0);
+		glVertex2f(0.0, 1.0);
+		glVertex2f(0.0, -1.0);
 
-	//	glfwSwapBuffers(window);
-	//	glfwPollEvents();
-	//}
+		// Draw part of simple harmonic motion equation
+		glColor3f(0.0, 1.0, 0.0);
+		for (float i = 0; i < 2 * 3.14159; i += 0.001) {
+			float x = (2 * i - 3.14159) / 3.14159;
+			float y = sin(i);
+			glVertex2f(x, y - 0.001);
+			glVertex2f(x, y);
+		}
+		glEnd();
 
-	//glfwTerminate();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	std::cout << std::endl;
 	std::cout << "########## END OF QUESTION 4 ##########" << std::endl;
-	std::cout << std::endl;
 
 	// end of question 4
 
